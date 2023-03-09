1af757446538b69923e3492a5ae92cfc252c9374
Mike Rachman <mikerachman1@gmail.com>
2023-03-09 13:17:20 -0500

    User can now register

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const UserModel = require("./models/User");
+
+require("dotenv").config();
 const app = express();
 
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+app.use(express.json());
 app.use(
   cors({
     credentials: true,
@@ -9,8 +17,27 @@ app.use(
   })
 );
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userDoc = await UserModel.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
+
 app.listen(4000);
+
+// jiM8YQrd2uVyS5f7
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
+import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+
+axios.defaults.baseURL = "http://localhost:4000";
 
 function App() {
   return (
