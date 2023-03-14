5cef5e6b97825a59ab85c8001fc2aa09e2e9e1c9
Mike Rachman <mikerachman1@gmail.com>
2023-03-14 14:24:46 -0400

    moved PhotoUploader to seperate comp

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import axios from "axios";
+
 import Perks from "../Perks";
+import PhotosUploader from "../PhotosUploader";
 
 export default function PlacesPage() {
   const { action } = useParams();
@@ -16,35 +17,6 @@ export default function PlacesPage() {
   const [checkIn, setCheckIn] = useState("");
   const [checkOut, setCheckOut] = useState("");
   const [maxGuests, setMaxGuests] = useState(1);
-
-  async function addPhotoByLink(e) {
-    e.preventDefault();
-    const { data: filename } = await axios.post("/upload-by-link", {
-      link: photoLink,
-    });
-    setAddedPhotos((prev) => {
-      return [...prev, filename];
-    });
-    setPhotoLink("");
-  }
-
-  function uploadPhoto(e) {
-    const files = e.target.files;
-    const data = new FormData();
-    for (let i = 0; i < files.length; i++) {
-      data.append("photos", files[i]);
-    }
-    axios
-      .post("/upload", data, {
-        headers: { "Content-Type": "multipart/form-data" },
-      })
-      .then((response) => {
-        const { data: filenames } = response;
-        setAddedPhotos((prev) => {
-          return [...prev, ...filenames];
-        });
-      });
-  }
 
   return (
     <div>
@@ -90,55 +62,12 @@ export default function PlacesPage() {
               onChange={(e) => setAddress(e.target.value)}
             />
             <h2 className="text-xl mt-4">Photos</h2>
-            <div className="flex gap-2">
-              <input
-                type="text"
-                placeholder={"Add using link ...jpg"}
-                value={photoLink}
-                onChange={(e) => setPhotoLink(e.target.value)}
-              />
-              <button
-                onClick={addPhotoByLink}
-                className="bg-gray-200 px-4 rounded-2xl"
-              >
-                Add&nbsp;photo
-              </button>
-            </div>
-            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-              {addedPhotos.length > 0 &&
-                addedPhotos.map((link) => (
-                  <div className="h-32 flex" key={link}>
-                    <img
-                      className="rounded-2xl w-full object-cover"
-                      src={"http://localhost:4000/uploads/" + link}
-                      alt="photo"
-                    />
-                  </div>
-                ))}
-              <label className="h-32 flex cursor-pointer items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
-                <input
-                  type="file"
-                  multiple
-                  className="hidden"
-                  onChange={uploadPhoto}
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
-                  />
-                </svg>
-                Upload
-              </label>
-            </div>
+            <PhotosUploader
+              addedPhotos={addedPhotos}
+              setAddedPhotos={setAddedPhotos}
+              photoLink={photoLink}
+              setPhotoLink={setPhotoLink}
+            />
             <h2 className="text-xl mt-4">Description</h2>
             <textarea
               value={description}
