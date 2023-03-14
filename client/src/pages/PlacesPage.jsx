32a287063d5935daf1ab8f8be2305f49e9b9f26d
Mike Rachman <mikerachman1@gmail.com>
2023-03-14 14:14:53 -0400

    moved Perks to seperate component

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import Perks from "../Perks";
 
 export default function PlacesPage() {
   const { action } = useParams();
@@ -43,15 +44,6 @@ export default function PlacesPage() {
           return [...prev, ...filenames];
         });
       });
-  }
-
-  function handleCbClick(ev) {
-    const { checked, name } = ev.target;
-    if (checked) {
-      setPerks([...perks, name]);
-    } else {
-      setPerks([...perks.filter((selectedName) => selectedName !== name)]);
-    }
   }
 
   return (
@@ -115,7 +107,7 @@ export default function PlacesPage() {
             <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
               {addedPhotos.length > 0 &&
                 addedPhotos.map((link) => (
-                  <div className="h-32 flex">
+                  <div className="h-32 flex" key={link}>
                     <img
                       className="rounded-2xl w-full object-cover"
                       src={"http://localhost:4000/uploads/" + link}
@@ -153,88 +145,7 @@ export default function PlacesPage() {
               onChange={(e) => setDescription(e.target.value)}
             />
             <h2 className="text-xl mt-4">Perks</h2>
-            <div className="grid gap-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6 mt-2">
-              <label className="border p-4 flex rounded-2xl gap-2 items-center">
-                <input type="checkbox" name="wifi" onChange={handleCbClick} />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
-                  />
-                </svg>
-
-                <span>Wifi</span>
-              </label>
-              <label className="border p-4 flex rounded-2xl gap-2 items-center">
-                <input
-                  type="checkbox"
-                  name="parking"
-                  onChange={handleCbClick}
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-                  />
-                </svg>
-
-                <span>Free Parking</span>
-              </label>
-              <label className="border p-4 flex rounded-2xl gap-2 items-center">
-                <input type="checkbox" name="pet" onChange={handleCbClick} />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-                  />
-                </svg>
-
-                <span>Pet Friendly</span>
-              </label>
-              <label className="border p-4 flex rounded-2xl gap-2 items-center">
-                <input type="checkbox" name="tv" onChange={handleCbClick} />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
-                  />
-                </svg>
-
-                <span>Tv</span>
-              </label>
-            </div>
+            <Perks perks={perks} setPerks={setPerks} />
             <h2 className="text-xl mt-4">Extra Info</h2>
             <textarea
               value={extraInfo}
