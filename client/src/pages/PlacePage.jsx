15f514848e7cb36b071de540932867b2a69fb63c
Mike Rachman <mikerachman1@gmail.com>
2023-03-23 14:33:24 -0400

    Place page info properly displayed

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import BookingWidget from "../BookingWidget";
+import PhotoGallery from "../PhotoGallery";
 
 export default function PlacePage() {
   const { id } = useParams();
@@ -19,47 +21,11 @@ export default function PlacePage() {
   if (!place) return "";
 
   if (showAllPhotos) {
-    return (
-      <div className="absolute inset-0 bg-black  min-h-screen">
-        <div className="bg-black p-8 grid gap-8">
-          <div>
-            <h2 className="text-3xl text-white">Photos of {place.title}</h2>
-            <button
-              onClick={() => setShowAllPhotos(false)}
-              className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              Close Photos
-            </button>
-          </div>
-          {place?.photos?.length > 0 &&
-            place.photos.map((photo, index) => (
-              <div key={index}>
-                <img
-                  className="rounded-2xl"
-                  src={"http://localhost:4000/uploads/" + photo}
-                  alt=""
-                />
-              </div>
-            ))}
-        </div>
-      </div>
-    );
+    return <PhotoGallery place={place} setShowAllPhotos={setShowAllPhotos} />;
   }
 
   return (
-    <div className="mt-4 bg-gray-100 mx-2 px-8 py-8">
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
       <h1 className="text-3xl">{place.title}</h1>
       <a
         className="flex gap-1 block font-semibold underline my-2"
@@ -93,7 +59,8 @@ export default function PlacePage() {
             {place.photos?.[0] && (
               <div>
                 <img
-                  className="aspect-square object-cover"
+                  onClick={() => setShowAllPhotos(true)}
+                  className="cursor-pointer aspect-square object-cover"
                   src={"http://localhost:4000/uploads/" + place.photos[0]}
                 />
               </div>
@@ -102,14 +69,16 @@ export default function PlacePage() {
           <div className="grid">
             {place.photos?.[1] && (
               <img
-                className="aspect-square object-cover"
+                onClick={() => setShowAllPhotos(true)}
+                className="cursor-pointer aspect-square object-cover"
                 src={"http://localhost:4000/uploads/" + place.photos[1]}
               />
             )}
             <div className="overflow-hidden">
               {place.photos?.[2] && (
                 <img
-                  className="aspect-square object-cover relative top-2"
+                  onClick={() => setShowAllPhotos(true)}
+                  className="cursor-pointer aspect-square object-cover relative top-2"
                   src={"http://localhost:4000/uploads/" + place.photos[2]}
                 />
               )}
@@ -135,19 +104,108 @@ export default function PlacePage() {
           Show more photos
         </button>
       </div>
-      <div className="my-4">
-        <h2 className="font-semibold text-2xl">Description</h2>
-        {place.description}
-      </div>
-      <div className="grid grid-cols-2">
+      <div className="mt-8 gap-8 grid grid-cols-1 md:grid-cols-[2fr_1fr]">
         <div>
+          <div className="my-4">
+            <h2 className="font-semibold text-2xl">Description</h2>
+            {place.description}
+          </div>
           Check-in: {place.checkIn}
           <br />
           Check-out: {place.checkOut}
           <br />
           Max Guests: {place.maxGuests}
         </div>
-        <div>b</div>
+        <div>
+          <BookingWidget place={place} />
+        </div>
+      </div>
+      <div>
+        <h2 className="mt-4 font-semibold text-2xl">Extra info</h2>
+      </div>
+      <div className="mb-4 mt-1 text-sm text-gray-700 leading-4">
+        {place.extraInfo}
+      </div>
+      <div>
+        <h2 className="mt-4 font-semibold text-2xl">Perks</h2>
+      </div>
+      <div className="flex gap-4 mb-4 mt-3 text-sm text-gray-700 leading-4">
+        {place.perks.includes("wifi") && (
+          <label className="border p-4 flex rounded-2xl gap-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
+              />
+            </svg>
+            <span>Wifi</span>
+          </label>
+        )}
+        {place.perks.includes("parking") && (
+          <label className="border p-4 flex rounded-2xl gap-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+              />
+            </svg>
+            <span>Free Parking</span>
+          </label>
+        )}
+        {place.perks.includes("pet") && (
+          <label className="border p-4 flex rounded-2xl gap-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+              />
+            </svg>
+            <span>Pet Friendly</span>
+          </label>
+        )}
+        {place.perks.includes("tv") && (
+          <label className="border p-4 flex rounded-2xl gap-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
+              />
+            </svg>
+            <span>Tv</span>
+          </label>
+        )}
       </div>
     </div>
   );
