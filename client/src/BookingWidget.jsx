9888199a4270e4e0ca625a073b9466b93141a3cc
Mike Rachman <mikerachman1@gmail.com>
2023-03-24 09:07:06 -0400

    states for booking created

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+import { differenceInCalendarDays } from "date-fns";
+
 export default function BookingWidget({ place }) {
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const [name, setName] = useState("");
+  const [mobile, setMobile] = useState("");
+
+  let numberOfNights = 0;
+  if (checkIn && checkOut) {
+    numberOfNights = differenceInCalendarDays(
+      new Date(checkOut),
+      new Date(checkIn)
+    );
+  }
+
   return (
     <div className="bg-white shadow p-4 rounded-2xl">
       <div className="text-2xl text-center">
@@ -8,19 +25,50 @@ export default function BookingWidget({ place }) {
         <div className="flex">
           <div className="p-4">
             <label>Check In:</label>
-            <input type="date" />
+            <input
+              type="date"
+              value={checkIn}
+              onChange={(e) => setCheckIn(e.target.value)}
+            />
           </div>
           <div className="p-4 border-l">
             <label>Check Out:</label>
-            <input type="date" />
+            <input
+              type="date"
+              value={checkOut}
+              onChange={(e) => setCheckOut(e.target.value)}
+            />
           </div>
         </div>
         <div className="p-4 border-t">
           <label>Number of Guests:</label>
-          <input type="number" value={1} />
+          <input
+            type="number"
+            value={numberOfGuests}
+            onChange={(e) => setNumberOfGuests(e.target.value)}
+          />
         </div>
+        {numberOfNights > 0 && (
+          <div className="p-4 border-t">
+            <label>Your full name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label>Phone number:</label>
+            <input
+              type="tel"
+              value={mobile}
+              onChange={(e) => setMobile(e.target.value)}
+            />
+          </div>
+        )}
       </div>
-      <button className="primary mt-4">Book this place</button>
+      <button className="primary mt-4">
+        Book this place
+        {numberOfNights > 0 && <span> ${numberOfNights * place.price}</span>}
+      </button>
     </div>
   );
 }
