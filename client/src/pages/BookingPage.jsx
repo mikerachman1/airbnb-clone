5290441243faa80fe0ece4082fb57902eede844c
Mike Rachman <mikerachman1@gmail.com>
2023-03-31 17:45:39 -0400

    single booking page html created

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import AddressLink from "../AddressLink";
+import PlaceGallery from "../PlaceGallery";
+import BookingDates from "../BookingDates";
 
 export default function BookingPage() {
+  const [booking, setBooking] = useState(null);
   const { id } = useParams();
-  return <div>single booking: {id}</div>;
+  useEffect(() => {
+    if (id) {
+      axios.get("/bookings").then((res) => {
+        const foundBooking = res.data.find(({ _id }) => _id === id);
+        if (foundBooking) {
+          setBooking(foundBooking);
+        }
+      });
+    }
+  }, [id]);
+
+  if (!booking) {
+    return "";
+  }
+  return (
+    <div className="my-8">
+      <h1 className="text-3xl">{booking.place.title}</h1>
+      <AddressLink>{booking.place.address}</AddressLink>
+      <div className="bg-gray-200 p-8 mb-4 rounded-2xl">
+        <h2 className="text-xl">Your booking info</h2>
+        <BookingDates booking={booking} />
+      </div>
+      <PlaceGallery place={booking.place} />
+    </div>
+  );
 }
